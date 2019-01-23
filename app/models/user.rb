2ed4b31795c247a42d259701d79ffcef0169f0c4
Suliman Alkous <sulimanalkous@gmail.com>
2019-01-23 20:36:33 +0300

    fix user.rb for ruby 2.5+ and rails 5.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
+  act_as_universal_and_determines_account
+
+  has_on :member, :dependent => :destroy
 end
